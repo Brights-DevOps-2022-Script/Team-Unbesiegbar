f2922d459819394f30bcbeac6d51ae2b4f7fac36
fschultes <felix.schultes@t-online.de>
2023-01-17 08:36:56 +0100

    scanner Post

--- Webblog/src/frontend/App.java
+++ Webblog/src/frontend/App.java
@@ -1,10 +1,20 @@
 package frontend;
+import java.util.Scanner;
 public class App {
     public static void main(String[] args) throws Exception {
         
-        Post firstpost = new Post("My First Post", 0001);
+        newpost();
 
-        firstpost.postInfo();
+    }
 
+public static void newpost () {
+    try (Scanner bookIn = new Scanner(System.in)) {
+        System.out.println("Please Enter title and postID");
+        String title = bookIn.nextLine();
+        int postID = bookIn.nextInt();
+        
+    Post post1 = new Post(title, postID);
+    post1.postInfo();
+    }
     }
 }
