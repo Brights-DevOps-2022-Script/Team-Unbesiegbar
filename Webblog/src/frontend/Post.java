07d5c752eee2805df9c209afbf8c8f1545144a38
fschultes <felix.schultes@t-online.de>
2023-01-17 08:02:38 +0100

    attributes and constr. post Class

---
+++ Webblog/src/frontend/Post.java
@@ -0,0 +1,11 @@
+package frontend;
+
+public class Post {
+    private String title;
+    private int postID;
+
+    public Post (String title, int postID){
+        title = this.title;
+        postID = this.postID;
+    }
+}
