0ec6f80a7903bfe72852efa41688c767d3cbcea2
fschultes <felix.schultes@t-online.de>
2023-01-17 08:13:38 +0100

    methode post print

--- Webblog/src/frontend/Post.java
+++ Webblog/src/frontend/Post.java
@@ -3,9 +3,14 @@ package frontend;
 public class Post {
     private String title;
     private int postID;
+    private String text;
+
 
     public Post (String title, int postID){
         title = this.title;
         postID = this.postID;
+    }
+    public void postInfo () {
+        System.out.println("TITEL: " + this.title + " PostID: " + this.postID);
     }
 }
