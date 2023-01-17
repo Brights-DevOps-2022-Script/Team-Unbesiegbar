3139cdc88d5a7131a9cbb5c73ac52e04966e97e3
fschultes <felix.schultes@t-online.de>
2023-01-17 18:14:23 +0100

    Datum Test und GetTest aus dem Constructor

---
+++ Webblog/src/frontend/PostTest.java
@@ -0,0 +1,24 @@
+package frontend;
+
+import java.beans.Transient;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class PostTest {
+    
+    @Test
+    public void getDate_Test() {
+        //Daten vorbereiten
+        Post post = new Post("Datum Test");
+        //Logik ausführen
+        
+        String date = post.getDate();
+        //Ergebnisse vergleichen
+        int datelength = date.length();
+        Assertions.assertEquals(19, datelength);
+        // test für Connection in neuer klasse Assertions.assertNotN
+        // insert () testen mit einem Post -> insert ausführen
+        // -> id vergleichen
+    }
+}
