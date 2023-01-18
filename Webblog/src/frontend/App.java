9a667c53bf38a3f3a2194b40eb02f8d3575d62d7
Pierre890 <pierfrancesco.elia89@gmail.com>
2023-01-18 10:15:58 +0100

    app

--- Webblog/src/frontend/App.java
+++ Webblog/src/frontend/App.java
@@ -1,6 +1,10 @@
 package frontend;
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        
+        Post firstpost = new Post("My First Post", 0001);
+
+        firstpost.postInfo();
+
     }
 }
