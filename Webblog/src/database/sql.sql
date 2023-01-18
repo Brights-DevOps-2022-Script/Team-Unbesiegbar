b0de3515a6b52a5a98e046ee97c8a5b226bd594f
RMN <rmn@users.noreply.github.com>
2023-01-18 15:59:34 +0100

    danke merkel

--- Webblog/src/database/sql.sql
+++ Webblog/src/database/sql.sql
@@ -1,6 +1,6 @@
 -- CREATE DATABASE pierixman;
 -- CREATE TABLE IF NOT EXISTS postMVP (postid integer PRIMARY KEY, title TEXT NOT NULL);
 -- INSERT INTO postMVP (postid, title) VALUES (1, "Start of Projekt Pierixman");
--- SELECT * FROM postMVP;
+SELECT * FROM postMVP;
 -- SELECT COUNT(postid) as count_post FROM postMVP;
 -- SELECT MAX (postID) as postID FROM postMVP;
