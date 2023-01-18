b6bc54fccf47fca99392fae44304854556559224
RMN <rmn@users.noreply.github.com>
2023-01-18 12:45:50 +0100

    ich fass nix mehr an

--- Webblog/src/frontend/App.java
+++ Webblog/src/frontend/App.java
@@ -11,7 +11,6 @@ public static void newpost () {
     try (Scanner postIn = new Scanner(System.in)) {
         System.out.println("Please Enter title and postID");
         String title = postIn.nextLine();
-        int postID = postIn.nextInt();
         
     Post post1 = new Post(title);
     post1.postInfo();
