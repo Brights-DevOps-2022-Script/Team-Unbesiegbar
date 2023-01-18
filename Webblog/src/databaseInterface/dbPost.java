3677803cc914ad40d30f6134fc1da89fd81f4b14
RMN <rmn@users.noreply.github.com>
2023-01-18 15:30:11 +0100

    nue

--- Webblog/src/databaseInterface/dbPost.java
+++ Webblog/src/databaseInterface/dbPost.java
@@ -6,10 +6,12 @@ public class dbPost {
     JavaConf myConf = new JavaConf();
     String myUrl = myConf.myUrl();
 
+    
+
     // insert zwei Mal vorhanden. 1. für Testzwecke. 2. kann Posttext empfangen
     public void insert(int postId, String title) {
         String sql = "INSERT INTO postMVP(postId, title) VALUES(?,?)";  
-        String url = myUrl(); 
+        String url = myUrl; 
         // Class.forName("org.sqlite.JDBC");
 
         try (Connection conn = DriverManager.getConnection(url);
