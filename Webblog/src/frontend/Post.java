8f92f1f196594355c6c2e004093a8116cdb9c3eb
Pierre890 <pierfrancesco.elia89@gmail.com>
2023-01-18 12:27:34 +0100

    app

--- Webblog/src/frontend/Post.java
+++ Webblog/src/frontend/Post.java
@@ -2,12 +2,13 @@ package frontend;
 import databaseInterface.*;
 public class Post {
     private String title;
-    private int postID = 0;
+    private int postID;
     private String text;
 
 
-    public Post (String title){
+    public Post (String title, int postID){
         this.title = title;
+        this.postID = postID;
     }
     public void postInfo () {
         System.out.println("TITEL: " + this.title + " PostID: " + this.postID);
