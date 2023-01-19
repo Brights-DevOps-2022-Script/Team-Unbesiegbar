a2b61cd6b990c9cdc2fccae74149914592901dd8
RMN <rmn@users.noreply.github.com>
2023-01-19 15:04:34 +0100

    dbComment update

--- Webblog/bin/database/sql.sql
+++ Webblog/bin/database/sql.sql
@@ -1,8 +1,10 @@
 -- CREATE DATABASE pierixman;
 -- CREATE TABLE IF NOT EXISTS postMVP (postid integer PRIMARY KEY, title TEXT NOT NULL);
--- INSERT INTO postMVP (postid, title) VALUES (1, "Start of Projekt Pierixman");
--- SELECT * FROM pierixmanOne;
+-- INSERT INTO commentTEST (commentID, contents, postid) VALUES (3, "something more", 4);
+-- SELECT * FROM commentTEST WHERE postid=4;
 -- SELECT COUNT(postid) as count_post FROM postMVP;
 -- SELECT MAX (postid) FROM postMVP;
+
+
 
 -- CREATE TABLE commentTEST(commentID integer NOT NULL, contents TEXT NOT NULL, postid integer, PRIMARY KEY(commentID), FOREIGN KEY (postid) REFERENCES pierixmanOne(postid));
