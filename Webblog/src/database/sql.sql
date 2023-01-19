0365b16078e5ded48636cd9a7683d337a252dea9
RMN <rmn@users.noreply.github.com>
2023-01-19 10:58:43 +0100

    showpost udpated

--- Webblog/src/database/sql.sql
+++ Webblog/src/database/sql.sql
@@ -7,7 +7,7 @@
 
 -- CREATE TABLE IF NOT EXISTS pierixmanOne (postid integer PRIMARY KEY, title TEXT NOT NULL, author TEXT NOT NULL, contents TEXT NOT NULL, date TEXT NOT NULL);
 -- INSERT INTO pierixmanOne (postid, title, author, contents, date) VALUES (3, "Eine Cubanische Cigarre wird erwachsen", "Felix C. S.", "Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed dia", "Gestern");
-SELECT * FROM pierixmanOne;
+SELECT * FROM pierixmanOne WHERE postid = 2;
 -- DROP TABLE IF EXISTS pierifmanOne;
 
 -- DELETE FROM pierixmanOne WHERE postid = 1;
