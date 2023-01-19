ff6a8c4883a2cc39b9556ebfc6c75bc8cec36d4f
RMN <rmn@users.noreply.github.com>
2023-01-19 11:27:49 +0100

    neuer Commit

--- Webblog/src/database/sql.sql
+++ Webblog/src/database/sql.sql
@@ -6,8 +6,8 @@
 -- SELECT MAX (postID) as postID FROM postMVP;
 
 -- CREATE TABLE IF NOT EXISTS pierixmanOne (postid integer PRIMARY KEY, title TEXT NOT NULL, author TEXT NOT NULL, contents TEXT NOT NULL, date TEXT NOT NULL);
--- INSERT INTO pierixmanOne (postid, title, author, contents, date) VALUES (3, "Eine Cubanische Cigarre wird erwachsen", "Felix C. S.", "Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed dia", "Gestern");
-SELECT * FROM pierixmanOne WHERE postid = 2;
+-- INSERT INTO pierixmanOne (postid, title, author, contents, date) VALUES (4, "Dicke Datteln am Strand", "Felix C. S.", "irgend ein TExt", "Datum" );
+SELECT * FROM pierixmanOne;
 -- DROP TABLE IF EXISTS pierifmanOne;
 
 -- DELETE FROM pierixmanOne WHERE postid = 1;
