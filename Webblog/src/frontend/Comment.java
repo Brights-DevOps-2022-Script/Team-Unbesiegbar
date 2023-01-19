66b42e8a164719be52096561b50d85272b0eb44b
fschultes <felix.schultes@t-online.de>
2023-01-19 14:10:08 +0100

    Comment Class

---
+++ Webblog/src/frontend/Comment.java
@@ -0,0 +1,13 @@
+package frontend;
+
+
+public class Comment {
+    private int commentID;
+    private String commentMessage;
+
+    public Comment(int commentID, String commentMessage) {
+        this.commentID= commentID;
+        this.commentMessage = commentMessage;
+    }
+
+}
