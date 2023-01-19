6b72cbe10b5423741eb2db881caecc4916915c76
fschultes <felix.schultes@t-online.de>
2023-01-19 14:45:27 +0100

    comment Class

--- Webblog/src/frontend/Comment.java
+++ Webblog/src/frontend/Comment.java
@@ -5,7 +5,6 @@ public class Comment {
     private int commentID;
     private String commentMessage;
 
-    Post post = new Post(3);
 
     public Comment(int commentID, String commentMessage, int postID) {
         this.commentID= commentID;
